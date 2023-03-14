d4f78e38631f07278cfd9bd997001ddf07cd2a2d
nick-haoran <33177696+nick-haoran@users.noreply.github.com>
2023-03-14 22:24:21 +0800

    Update google.py

--- google.py
+++ google.py
@@ -3,7 +3,7 @@ import sys
 
 import requests
 
-url = f"https://www.googleapis.com/customsearch/v1?key=AIzaSyCjzhRGOwdo_b3bdrW0t8-fMCPh8GbE4Aw&q={sys.argv[1]}&cx=c2e9aa4a7fd614ecc&num=5&hl=zh-CN"
+url = f"https://www.googleapis.com/customsearch/v1?key=*******************************&q={sys.argv[1]}&cx=*********************&num=5&hl=zh-CN"
 response = requests.get(url)
 result = json.loads(response.text.replace('\xa0', ' ').replace('\u2022', ' '))
 
